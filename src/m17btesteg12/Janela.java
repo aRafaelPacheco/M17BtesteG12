9074b4fceee35f152ef08802b934a28d2b515684
a18986 <a18986@d1-08-10.dped.local>
2017-02-14 16:04:37 +0000

    versão 1.0

---
+++ src/m17btesteg12/Janela.java
@@ -0,0 +1,153 @@
+package m17btesteg12;
+/**
+ * 
+ * @author Rafael Pacheco
+ * @version 1.0 _14 2 17_
+ * 
+ * 
+ */
+public class Janela extends javax.swing.JFrame {
+
+   public Janela() {
+      initComponents();
+   }
+
+   @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        campoNumero = new javax.swing.JTextField();
+        botaoVerificar = new javax.swing.JButton();
+        labelPrimoSN = new javax.swing.JLabel();
+        clear = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Verificar se um número é primo");
+
+        jLabel1.setText("Número: ");
+
+        campoNumero.setHorizontalAlignment(javax.swing.JTextField.RIGHT);
+
+        botaoVerificar.setText("Verificar...");
+        botaoVerificar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botaoVerificarActionPerformed(evt);
+            }
+        });
+
+        clear.setText("clear");
+        clear.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                clearActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(132, 132, 132)
+                        .addComponent(botaoVerificar)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(clear))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(campoNumero, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(18, 18, 18)
+                .addComponent(labelPrimoSN, javax.swing.GroupLayout.PREFERRED_SIZE, 151, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(campoNumero, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(labelPrimoSN))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(botaoVerificar)
+                    .addComponent(clear))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+   private void botaoVerificarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botaoVerificarActionPerformed
+      
+      try{
+        long numero = Integer.parseInt(campoNumero.getText());
+        int i;
+        boolean bandeira;
+        bandeira = true;
+        for(i=2;i<numero;i++){
+           if(numero%i == 0){
+              bandeira = false;
+           }
+        }
+        if(bandeira){
+           labelPrimoSN.setText("é primo");      
+        }
+        else{
+           labelPrimoSN.setText("não é primo");      
+        }
+      }
+      catch(NumberFormatException e){
+          campoNumero.setText("Número introduzido INVÁLIDO");
+      }
+   }//GEN-LAST:event_botaoVerificarActionPerformed
+
+    private void clearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_clearActionPerformed
+        // TODO add your handling code here:
+        campoNumero.setText("");
+        labelPrimoSN.setText("");
+        
+    }//GEN-LAST:event_clearActionPerformed
+
+   public static void main(String args[]) {
+      /* Set the Nimbus look and feel */
+      //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+       * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+       */
+      try {
+         for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+            if ("Nimbus".equals(info.getName())) {
+               javax.swing.UIManager.setLookAndFeel(info.getClassName());
+               break;
+            }
+         }
+      } catch (ClassNotFoundException ex) {
+         java.util.logging.Logger.getLogger(Janela.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+      } catch (InstantiationException ex) {
+         java.util.logging.Logger.getLogger(Janela.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+      } catch (IllegalAccessException ex) {
+         java.util.logging.Logger.getLogger(Janela.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+      } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+         java.util.logging.Logger.getLogger(Janela.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+      }
+      //</editor-fold>
+
+      /* Create and display the form */
+      java.awt.EventQueue.invokeLater(new Runnable() {
+         @Override
+         public void run() {
+            new Janela().setVisible(true);
+         }
+      });
+   }
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton botaoVerificar;
+    private javax.swing.JTextField campoNumero;
+    private javax.swing.JButton clear;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel labelPrimoSN;
+    // End of variables declaration//GEN-END:variables
+}
